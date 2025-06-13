afcf9e0c2d85bb194aa3d0715ea297a3a008767c
Android Studio <devandroid@ort>
2025-06-13 11:59:07 -0300

    hay un bug

--- src/configs/db-config.js
+++ src/configs/db-config.js
@@ -1,8 +1,8 @@
 const config = {
-    host: "localhost",
-    database: "script-postgress",
-    user: "postgres",
-    password: "root",
-    port: 3000
+    host: "aws-0-sa-east-1.pooler.supabase.com",
+    database: "postgres",
+    user: "postgres.cmomapphxafnzknjudsr",
+    password: "@Databasetp10pati",
+    port: 5342
 }
 export default config;
--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.use(express.json()); // Middleware para parsear y comprender JSON
 
 //
 
-app.get('/api/alumnos/', async (req, res) => {
+app.get('/api/alumnos', async (req, res) => {
     try {
         const client = new Client(config);
         await client.connect();
@@ -41,7 +41,7 @@ app.get('/api/alumnos/', async (req, res) => {
         res.status(200).json(result.rows);
     }
     catch (error){
-        res.status(500).send("(Internal Server Error) error: " + error.message)
+        res.status(500).send("(Internal Server Error) error: " + error.message + "casi")
     }
 })
 
